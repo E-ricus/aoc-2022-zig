914776526c85d1d709c88c5452a271841b19d5c4
Eric Puentes <eric.david2333@gmail.com>
2022-12-16 19:17:06 -0500

    fix memory leak

--- src/day1.zig
+++ src/day1.zig
@@ -13,7 +13,6 @@ pub fn run(allocator: std.mem.Allocator) !void {
 
 fn parseElfs(content: []const u8, allocator: std.mem.Allocator) ![]usize {
     var elfs = std.ArrayList(usize).init(allocator);
-    defer elfs.deinit();
     var lines = std.mem.split(u8, content, "\n");
     var elf: usize = 0;
     while (lines.next()) |line| {
@@ -29,12 +28,14 @@ fn parseElfs(content: []const u8, allocator: std.mem.Allocator) ![]usize {
 
 fn maxCalories(content: []const u8, allocator: std.mem.Allocator) !usize {
     const elfs = try parseElfs(content, allocator);
+    defer allocator.free(elfs);
     return std.mem.max(usize, elfs);
 }
 
 fn sumCalories(content: []const u8, allocator: std.mem.Allocator) !usize {
     var sum: usize = 0;
     const elfs = try parseElfs(content, allocator);
+    defer allocator.free(elfs);
     std.sort.sort(usize, elfs, {}, std.sort.desc(usize));
     for (elfs[0..3]) |p| {
         sum += p;
@@ -44,34 +45,22 @@ fn sumCalories(content: []const u8, allocator: std.mem.Allocator) !usize {
 
 const test_content = @embedFile("inputs/day1.test");
 
-// TODO: The test allocator is detecting leaks investigate why.
-// const test_allocator = std.testing.allocator;
+const test_allocator = std.testing.allocator;
 
 test "test parseElfs" {
-    // TODO: Delete when the test allocator works
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const test_allocator = arena.allocator();
     const elfs = try parseElfs(test_content, test_allocator);
+    defer test_allocator.free(elfs);
     try std.testing.expectEqual(@as(usize, 5), elfs.len);
     try std.testing.expectEqual(@as(usize, 6000), elfs[0]);
     try std.testing.expectEqual(@as(usize, 10000), elfs[4]);
 }
 
 test "test maxCalories" {
-    // TODO: Delete when the test allocator works
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const test_allocator = arena.allocator();
     const max = try maxCalories(test_content, test_allocator);
     try std.testing.expectEqual(@as(usize, 24000), max);
 }
 
 test "test sumCalories" {
-    // TODO: Delete when the test allocator works
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const test_allocator = arena.allocator();
     const sum = try sumCalories(test_content, test_allocator);
     try std.testing.expectEqual(@as(usize, 45000), sum);
 }
