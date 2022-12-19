f6e245fbb31561f1ddb50478bdbf3e299068279a
Eric Puentes <eric.david2333@gmail.com>
2022-12-19 11:12:13 -0500

    day 4

---
+++ src/day4.zig
@@ -0,0 +1,87 @@
+const std = @import("std");
+const print = std.debug.print;
+
+// Day input
+const day_content = @embedFile("inputs/day4.input");
+
+pub fn run() !void {
+    const cont = try containedOrOverlaped(day_content, true);
+    print("Groups contained: {d}", .{cont});
+    const over = try containedOrOverlaped(day_content, false);
+    print("Groups contained: {d}", .{over});
+}
+
+fn containedOrOverlaped(content: []const u8, contained: bool) !usize {
+    var lines = std.mem.split(u8, std.mem.trimRight(u8, content, "\n"), "\n");
+    var sum: usize = 0;
+    while (lines.next()) |line| {
+        var split = std.mem.split(u8, line, ",");
+        var first = split.next().?;
+        var second = split.next().?;
+        if (try verify(first, second, contained)) {
+            sum += 1;
+        }
+    }
+    return sum;
+}
+
+fn verify(first: []const u8, second: []const u8, contained: bool) !bool {
+    var split = std.mem.split(u8, first, "-");
+    const startFirst = try std.fmt.parseInt(usize, split.next().?, 10);
+    const finishFirst = try std.fmt.parseInt(usize, split.next().?, 10);
+    split = std.mem.split(u8, second, "-");
+    const startSecond = try std.fmt.parseInt(usize, split.next().?, 10);
+    const finishSecond = try std.fmt.parseInt(usize, split.next().?, 10);
+    if (contained) {
+        if (startFirst >= startSecond and finishFirst <= finishSecond) {
+            return true;
+        }
+        if (startSecond >= startFirst and finishSecond <= finishFirst) {
+            return true;
+        }
+        return false;
+    }
+    if (startFirst >= startSecond and startFirst <= finishSecond) {
+        return true;
+    }
+    if (finishFirst >= startSecond and finishFirst <= finishSecond) {
+        return true;
+    }
+    if (startSecond >= startFirst and startSecond <= finishFirst) {
+        return true;
+    }
+    if (finishSecond >= startFirst and finishSecond <= finishFirst) {
+        return true;
+    }
+    return false;
+}
+
+const test_content = @embedFile("inputs/day4.test");
+
+test "test verify contained" {
+    var is_contained = try verify("2-8", "3-7", true);
+    try std.testing.expect(is_contained);
+    is_contained = try verify("6-6", "4-6", true);
+    try std.testing.expect(is_contained);
+    is_contained = try verify("2-4", "6-8", true);
+    try std.testing.expect(!is_contained);
+}
+
+test "test verify overlaped" {
+    var is_overlaped = try verify("5-7", "7-9", false);
+    try std.testing.expect(is_overlaped);
+    is_overlaped = try verify("6-6", "4-6", false);
+    try std.testing.expect(is_overlaped);
+    is_overlaped = try verify("2-3", "4-5", false);
+    try std.testing.expect(!is_overlaped);
+}
+
+test "test contained" {
+    const sum = try containedOrOverlaped(test_content, true);
+    try std.testing.expectEqual(@as(usize, 2), sum);
+}
+
+test "test overlaped" {
+    const sum = try containedOrOverlaped(test_content, false);
+    try std.testing.expectEqual(@as(usize, 4), sum);
+}
--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const print = std.debug.print;
 const day1 = @import("day1.zig");
 const day2 = @import("day2.zig");
 const day3 = @import("day3.zig");
+const day4 = @import("day4.zig");
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -12,6 +13,7 @@ pub fn main() !void {
     try day1.run(alloc);
     day2.run();
     try day3.run(alloc);
+    try day4.run();
 }
 
 test {
